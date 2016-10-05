cf86e04526d56e8fb6e1de9797bd939dd9d0af0c
Shutaro Takimoto <shu@tak.im>
2016-10-05 23:42:34 +0900

    feat: add badge

--- update.js
+++ update.js
@@ -3,6 +3,7 @@ const path = require('path');
 const fs = require('fs');
 const packageJson = require('./package.json');
 const urls = require('./url.json');
+const badges = require('./readmeBadges.json');
 
 const readmeFile = path.resolve(__dirname) + '/README.md';
 
@@ -13,7 +14,7 @@ function zip(a1, a2) {
 const pages = Object.keys(urls);
 const promises = pages.map(k => cli.execute(k, 'md'))
 Promise.all(promises).then(contents => {
-  const header = '# ' + packageJson.name;
+  const header = '# ' + packageJson.name + '\n' + badges.join('\n');
   const joinedContents = zip(pages, contents).reduce((prev, [page, content]) =>
     prev + '\n\n## ' + page.toUpperCase() + '\n\n' + content
   , '');
