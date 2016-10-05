bed4ed13427cf473a5c0aa3c387027d43e62babf
Shutaro Takimoto <shu@tak.im>
2016-10-05 23:19:32 +0900

    feat: add feature to update README.md

---
+++ update.js
@@ -0,0 +1,23 @@
+const cli = require('./cli');
+const path = require('path');
+const fs = require('fs');
+const packageJson = require('./package.json');
+const urls = require('./url.json');
+
+const readmeFile = path.resolve(__dirname) + '/README.md';
+
+function zip(a1, a2) {
+  return a1.map((e, i) => [e, a2[i]]);
+}
+
+const pages = Object.keys(urls);
+const promises = pages.map(k => cli.execute(k, 'md'))
+Promise.all(promises).then(contents => {
+  const header = '# ' + packageJson.name;
+  const joinedContents = zip(pages, contents).reduce((prev, [page, content]) =>
+    prev + '\n\n## ' + page.toUpperCase() + '\n\n' + content
+  , '');
+
+  fs.writeFileSync(readmeFile, header + joinedContents);
+  console.log('Updated successfully');
+}).catch(error => console.error(error));
