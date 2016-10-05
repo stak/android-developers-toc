ae2efd5f9888bdea95f2657c66945c1431be49c2
Shutaro Takimoto <shu@tak.im>
2016-10-05 19:57:29 +0900

    feat: add cli

---
+++ cli.js
@@ -0,0 +1,46 @@
+const program = require('commander');
+const getToc = require('./index');
+const package = require('./package.json');
+const urls = require('./url.json');
+
+function addIndent(str, indent = 4) {
+  return str.split('\n')
+            .map(line => ' '.repeat(indent) + line)
+            .join('\n');
+}
+
+function toMarkdown(toc) {
+  return toc.map(item =>
+    `- [${item.title}](${item.url})` + (item.children ?
+                                       '\n' + addIndent(toMarkdown(item.children)) :
+                                       '')).join('\n');
+}
+
+program.version(package.version)
+       .usage(`[options] <target: ${Object.keys(urls).join(', ')}>`)
+       .description('Fetch and scrape https://developer.android.com/')
+       .option('-t, --type [type]', 'Output format', /^(json|md)$/i, 'json')
+       .parse(process.argv);
+
+if (!program.args.length) {
+    program.help();
+    return;
+}
+
+getToc(program.args).then(v => {
+  switch (program.type) {
+  case 'json':
+    process.stdout.write(JSON.stringify(v));
+    break;
+  case 'md':
+    process.stdout.write(toMarkdown(v));
+    break;
+  default:
+    process.stderr.write('invalid format was specified');
+    process.exit(1);
+    break;
+  }
+}).catch(err => {
+  process.stderr.write(`failed to fetch "${program.args}"`);
+  process.exit(1);
+});
